90910c3055a6332218abd8bd8c7b9c436a954da3
Limpid <Limpid8818@outlook.com>
2026-01-14 22:26:32 +0800

    feat: storage util

---
+++ lib/core/utils/storage_util.dart
@@ -0,0 +1,42 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageUtil {
+  static late SharedPreferences _prefs;
+
+  // 初始化方法
+  static Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  // Token 相关
+  static const String _keyToken = "token";
+
+  static Future<bool> setToken(String value) =>
+      _prefs.setString(_keyToken, value);
+  static String? getToken() => _prefs.getString(_keyToken);
+  static Future<bool> removeToken() => _prefs.remove(_keyToken);
+
+  // 用户 ID 用户名 相关
+  static const String _keyUserId = "userId";
+
+  static Future<bool> setUserId(String value) =>
+      _prefs.setString(_keyUserId, value);
+  static String? getUserId() => _prefs.getString(_keyUserId);
+
+  static const String _keyUsername = "username";
+
+  static Future<bool> setUsername(String value) =>
+      _prefs.setString(_keyUsername, value);
+  static String? getUsername() => _prefs.getString(_keyUsername);
+
+  static const String _keyAvatar = "avatar_url";
+
+  static Future<bool> setAvatar(String value) =>
+      _prefs.setString(_keyAvatar, value);
+  static String? getAvatar() => _prefs.getString(_keyAvatar);
+
+  // 退出登录清理缓存
+  static Future<void> clearAll() async {
+    await _prefs.clear();
+  }
+}
